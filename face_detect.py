3ea867db7da8e9fda1dec90e23ce4f593dce75df
Eukleides <pavlosmavrikis@hotmail.com>
2019-06-12 21:21:44 +0100

    Delete face_detect.py

--- face_detect.py
+++
@@ -1,8 +0,0 @@
-
-import data_load as dl
-
-dl.set_seed()
-fd = dl.FDDB_dataset()
-X_train, X_test, y_train, y_test = fd.generate_training_data(verbose=1)
-
-fd.display_image(X_train[10], y_train[10])
